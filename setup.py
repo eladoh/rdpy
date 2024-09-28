c2ff8f2a010fbbf517b9a082d87f4e006b67f178
eladoh <eladoh244@gmail.com>
2024-09-28 19:44:05 +0300

    bhrthrth

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
-import setuptools
-from distutils.core import setup, Extension
+# import setuptools
+# from distutils.core import setup, Extension
+from setuptools import setup, Extension
+
 
 setup(name='rdpy',
 	version='1.3.2',
